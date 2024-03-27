8b966f1922a59c9e49bce7e8dbc24558f156dccf
huanfeng <yeh.hua@northeastern.edu>
2024-03-26 17:25:38 -0700

    Initil commit

---
+++ src/main/java/Seam/Intern_Coding_Assignment/InternCodingAssignmentApplication.java
@@ -0,0 +1,13 @@
+package Seam.Intern_Coding_Assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternCodingAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternCodingAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Seam/Intern_Coding_Assignment/InternCodingAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package Seam.Intern_Coding_Assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternCodingAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
